3dc8dd3f92fabc84adf5e02e5ac746c40444aa4f
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-08-13 19:40:00 +1000

    Add Dockerfile

--- src/main/java/com/comp4050square/testExecutor/controller/TestController.java
+++ src/main/java/com/comp4050square/testExecutor/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
@@ -16,15 +15,24 @@ import java.io.*;
 public class TestController {
     String bucketName = "uploads-76078f4";
 
-    static class TestDetails {
+     static class TestDetails {
         String s3Key;
-    }
+
+
+         public String getS3Key() {
+             return s3Key;
+         }
+
+         public void setS3Key(String s3Key) {
+             this.s3Key = s3Key;
+         }
+     }
 
     @PostMapping("/")
     public String runTest(@RequestBody TestDetails testDetails) {
         System.out.println("Running test");
 
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_SOUTHEAST_2).build();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
 
         try {
             File outFile = new File("/tmp/tmpfile.txt");
