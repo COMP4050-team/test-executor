a75f4cf5a093badc65472301c7986e30aa836d53
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-08-25 21:47:22 +1000

    Add S3Client and refactor test controller

--- src/main/java/com/comp4050square/testExecutor/controller/TestController.java
+++ src/main/java/com/comp4050square/testExecutor/controller/TestController.java
@@ -1,106 +1,96 @@
 package com.comp4050square.testExecutor.controller;
 
+import com.amazonaws.AmazonServiceException;
+import com.comp4050square.testExecutor.S3Client;
 import com.comp4050square.testExecutor.parser.TestParser;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.*;
-import org.springframework.web.server.ResponseStatusException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
 
-import java.io.*;
 
 
 @RestController
 public class TestController {
+
+    static class TestDetails {
+        String s3KeyTestFile;
+        String s3KeyProjectFile;
+
+        public String getS3KeyTestFile() {
+            return s3KeyTestFile;
+        }
+
+        public void setS3KeyTestFile(String s3KeyTestFile) {
+            this.s3KeyTestFile = s3KeyTestFile;
+        }
+
+        public String getS3KeyProjectFileKeyTestFile() {
+            return s3KeyProjectFile;
+        }
+
+        public void setS3KeyProjectFile(String s3KeyProjectFile) {
+            this.s3KeyProjectFile = s3KeyProjectFile;
+        }
+    }
     String bucketName = "uploads-76078f4";
 
-     static class TestDetails {
-         String s3KeyTestFile;
-         String s3KeyProjectFile;
+    private String readFile(String filePath) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
 
-         public String getS3KeyTestFile() {
-             return s3KeyTestFile;
-         }
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
 
-         public void setS3KeyTestFile(String s3KeyTestFile) {
-             this.s3KeyTestFile = s3KeyTestFile;
-         }
+        return sb.toString();
+    }
 
-         public String getS3KeyProjectFileKeyTestFile() {
-             return s3KeyProjectFile;
-         }
-
-         public void setS3KeyProjectFile(String s3KeyProjectFile) {
-             this.s3KeyProjectFile = s3KeyProjectFile;
-         }
-     }
 
     @PostMapping("/")
     public String runTest(@RequestBody TestDetails testDetails) {
 
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+        final S3Client s3Client = new S3Client(bucketName);
 
         try {
-            //storing the test file
-            File outFile = new File("/tmp/tmpTest.txt");
-            ObjectMetadata metadata = s3.getObject(new GetObjectRequest(bucketName, testDetails.s3KeyTestFile), outFile);
-            if (metadata == null) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Test Key does not exist");
-            }
+            // Storing the test file
+            s3Client.downloadFile(testDetails.s3KeyTestFile, "/tmp/tmpTest.txt");
 
-            //Getting list of files inside the projects
-            ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName).withPrefix(testDetails.s3KeyProjectFile).withDelimiter("/");
-            ListObjectsV2Result listing = s3.listObjectsV2(req);
+            // Getting list of files inside the projects
+            Set<String> projectFiles = s3Client.listObjects(testDetails.s3KeyProjectFile);
 
-            //storing the files locally
-            System.out.println("Downloading file");
-            for (S3ObjectSummary summary: listing.getObjectSummaries()) {
-                if(summary.getKey().equals(testDetails.s3KeyProjectFile)){
+            // Storing the files locally
+            for (String s3Key : projectFiles) {
+                if (s3Key.equals(testDetails.s3KeyProjectFile)) {
                     continue;
                 }
-                
-                //making files in local directory and copying from s3 to local files
-                String projectFileName = "/tmp/" + summary.getKey();
-                File projectFile = new File(projectFileName);
-                s3.getObject(new GetObjectRequest(bucketName, summary.getKey()), projectFile);
 
-                //parsing the project file and creating the corresponding java file
+                // Making files in local directory and copying from s3 to local files
+                String projectFileName = "/tmp/" + s3Key;
+                s3Client.downloadFile(s3Key, projectFileName);
+
+                // Parsing the project file and creating the corresponding java file
                 TestParser t = new TestParser(projectFileName);
                 t.readFile();
                 t.createFile();
             }
 
-            System.out.println("Running test");
-
-            /*
-            printing output to client
-             */
-            try (BufferedReader br = new BufferedReader(new FileReader("/tmp/tmpTest.txt"))) {
-                StringBuilder sb = new StringBuilder();
-                String line = br.readLine();
-
-                while (line != null) {
-                    sb.append(line);
-                    sb.append(System.lineSeparator());
-                    line = br.readLine();
-                }
-                return sb.toString();
-            }
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
+            // Return file content
+            return readFile("/tmp/tmpTest.txt");
+        } catch (AmazonServiceException | IOException e) {
+            System.err.println(e.getMessage());
 
             System.out.println("Failed to save file");
 
             return "500 BAD";
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
-
 }
 
