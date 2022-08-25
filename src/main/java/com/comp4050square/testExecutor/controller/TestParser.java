e3d2fa9f8975234f186d7ed7a4be7c1d7b677cef
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-08-25 21:28:41 +1000

    Keep code objects as long as possible

--- src/main/java/com/comp4050square/testExecutor/controller/TestParser.java
+++ src/main/java/com/comp4050square/testExecutor/controller/TestParser.java
@@ -1,77 +1,98 @@
 package com.comp4050square.testExecutor.controller;
 
 import spoon.Launcher;
+import spoon.reflect.code.CtStatement;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtField;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.ModifierKind;
+import spoon.support.reflect.code.CtInvocationImpl;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class TestParser {
 
-    private String path;
+    private final Path path;
     private String fileName;
-    private String globalVariable;
-    private String drawBlock;
-    private String setUpBlock;
-    private String otherBlocks;
+    private ArrayList<CtField<?>> globalVariables;
+    private CtMethod<?> drawBlock;
+    private CtMethod<?> setUpBlock;
+    private HashSet<CtMethod<?>> methods;
 
-    TestParser(String p){
-        this.path = p;
+    public TestParser(String p) {
+        this.path = Path.of(p);
+    }
+
+    private String getFilenameNoExtension(String fileName) {
+        int lastPeriodPos = fileName.lastIndexOf('.');
+
+        if (lastPeriodPos == -1) {
+            return fileName;
+        } else {
+            return fileName.substring(0, lastPeriodPos);
+        }
     }
 
     public void readFile() throws IOException {
 
-        //Read the file in the given path and create a CtClass to read the content
-        File file = new File(path);
-        String fileContent = Files.readString(Path.of(path), StandardCharsets.US_ASCII);
-        CtClass l = Launcher.parseClass("class Processing {" + fileContent + "}");
+        // Read the file in the given path and create a CtClass to read the content
+        fileName = getFilenameNoExtension(path.getFileName().toString());
 
-        //Store draw and setup block in corresponding string
-        drawBlock = String.valueOf(l.getMethod("draw"));
-        setUpBlock = String.valueOf(l.getMethod("setup"));
+        String fileContent = Files.readString(path, StandardCharsets.US_ASCII);
+        CtClass<?> l = Launcher.parseClass("class " + fileName + " {" + fileContent + "}");
 
-        //Getting the fileName from the path using spliter
-        String[] temp = path.split("/");
-        fileName = temp[temp.length-1].replace(".pde", "");
+        // Store draw and setup block in corresponding string
+        drawBlock = l.getMethod("draw");
+        setUpBlock = l.getMethod("setup");
 
-        //Getting global variables
-        List fields = l.getFields();
-        for (int i = 0; i < fields.size(); i++) {
-            globalVariable += fields.get(i);
+        // Getting global variables
+        globalVariables = new ArrayList<>(l.getFields());
+
+        // Getting all the other methods
+        methods = new HashSet<>(l.getMethods());
+
+        // Remove setup
+        methods.removeIf(ctMethod -> ctMethod.getSimpleName().equals("setup"));
+
+        // Remove draw
+        methods.removeIf(ctMethod -> ctMethod.getSimpleName().equals("draw"));
+
+        // Set all methods to public
+        methods.forEach(method -> method.setVisibility(ModifierKind.PUBLIC));
+    }
+
+    private String codeObjectListToString(Collection<?> col) {
+        if (col.size() == 0) {
+            return "";
         }
 
-        //Getting all the other methods
-        ArrayList method = new ArrayList<>(l.getMethods());
-        method.remove(0);
-        method.remove(method.size() - 1);
-        for (Object x : method) {
-            otherBlocks += x;
-            otherBlocks += "\n";
-        }
-
-        //removing unnecessary null and replacing void to public void
-        globalVariable = globalVariable.replaceFirst("null", "");
-        otherBlocks = otherBlocks.replaceFirst("null", "");
-        otherBlocks = otherBlocks.replace("void", "public void");
+        return col.stream().map(Object::toString).collect(Collectors.joining("\n"));
     }
 
     public void createFile() throws IOException {
-        //Produce destination path
-        String des = path.replace("projects", "content");
-        des = des.replace(".pde", ".java");
+        // Get the size variable and remove it from the setUpBlock
+        ArrayList<CtStatement> setupStatements = new ArrayList<>();
+        CtStatement sizeStatement = null;
+        for (CtStatement statement : setUpBlock.getBody().getStatements()) {
+            if (statement instanceof CtInvocationImpl) {
+                if (((CtInvocationImpl<?>) statement).getExecutable().getSimpleName().equals("size")) {
+                    sizeStatement = statement;
+                    continue;
+                }
+            }
 
-        //Get the size variable and remove it from the setUpBlock
-        String[] temp = setUpBlock.split("\n");
-        String sizeVariable = temp[1];
-        setUpBlock = setUpBlock.replace(sizeVariable, "");
+            setupStatements.add(statement);
+        }
 
-        FileWriter outFile = new FileWriter(des);
+        FileWriter outFile = new FileWriter(String.format("/tmp/%s.java", fileName));
         outFile.write(
                 "import processing.core.*; \n" +
                         "import processing.data.*; \n" +
@@ -89,14 +110,14 @@ public class TestParser {
                         "\n" +
                         String.format("public class %s extends PApplet {\n", fileName) +
                         "\n" +
-                        String.format("%s", globalVariable) +
+                        String.format("%s", codeObjectListToString(globalVariables)) +
                         "\n" +
-                        String.format("public %s\n", setUpBlock) +
+                        String.format("public void setup() {%s}\n", codeObjectListToString(setupStatements)) +
                         "\n" +
                         String.format("public %s\n", drawBlock) +
                         "\n" +
-                        String.format("%s", otherBlocks) +
-                        String.format("  public void settings() {%s}\n", sizeVariable) +
+                        String.format("%s", codeObjectListToString(methods)) +
+                        String.format("public void settings() { %s; }\n", sizeStatement) +
                         "  static public void main(String[] passedArgs) {\n" +
                         String.format("    String[] appletArgs = new String[] { \"%s\" };\n", fileName) +
                         "    if (passedArgs != null) {\n" +
