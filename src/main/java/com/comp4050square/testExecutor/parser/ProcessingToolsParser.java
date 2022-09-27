f7b880ddf9da0e22858f0b194dcf32f8ea6e0242
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2022-09-27 18:49:12 +1000

    Use process builder

--- src/main/java/com/comp4050square/testExecutor/parser/ProcessingToolsParser.java
+++ src/main/java/com/comp4050square/testExecutor/parser/ProcessingToolsParser.java
@@ -9,34 +9,44 @@ public class ProcessingToolsParser {
     private String readStream(InputStream stream) {
         StringBuilder result = new StringBuilder();
 
-        Scanner scanner = new Scanner(stream).useDelimiter("\\n");
-
-        while (scanner.hasNext()) {
-            result.append(scanner.next());
+        try (Scanner scanner = new Scanner(stream).useDelimiter("\\n")) {
+            while (scanner.hasNext()) {
+                result.append(scanner.next());
+            }
         }
 
         return result.toString();
     }
 
     public Boolean parse(String path) {
-        boolean compileError = false;
-        String[] pathList = path.split("/");
-        String outPath = String.format("/tmp/out/%s/%s", pathList[pathList.length - 2], pathList[pathList.length - 1]);
-
-        System.out.println("running processing-java");
         try {
-            Process process = Runtime.getRuntime().exec(new String[]{"/usr/local/bin/processing/processing-java", "--force", "--sketch=" + path, "--output=" + outPath, "--build"});
+            boolean compileError = false;
+            String[] pathList = path.split("/");
+            String outPath = String.format("/tmp/out/%s/%s", pathList[pathList.length - 2],
+                    pathList[pathList.length - 1]);
+
+            System.out.println("running processing-java");
+
+            ProcessBuilder builder = new ProcessBuilder();
+            builder.command("sh", "-c",
+                    "/usr/local/bin/processing/processing-java",
+                    "--force",
+                    "--sketch=" + path + "/",
+                    "--output=" + outPath,
+                    "--build");
+            Process process = builder.start();
             InputStream serrStream = process.getErrorStream();
-
             String errString = readStream(serrStream);
+            System.out.println(errString);
 
+            process.waitFor();
             if (errString.length() != 0) {
                 compileError = true;
             }
             System.out.println("finished processing-java");
 
             return !compileError;
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             System.out.println("error running processing java");
             System.out.println(e);
             return false;
